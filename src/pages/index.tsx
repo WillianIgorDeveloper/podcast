9777765ce70919c31efba4915d18fb45ba82b7f7
Willian Igor <yellowmoonastronaut@gmail.com>
2022-09-07 16:06:39 -0300

    add json-server and server.json file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
-export default function Index () {
+export default function Index (props) {
     return (
         <>
             <h1>Seja bem vindo!</h1>
+            <p>{JSON.stringify(props.epsodes)}</p>
         </>
     )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return {
+        props: {
+            epsodes: data,
+        }
+    }
 }
